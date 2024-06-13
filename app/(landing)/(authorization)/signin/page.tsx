24e462b35bd1c0cea01923684fc330b3aa948e54
Kuba <kubalew.pl@gmail.com>
2024-06-13 13:06:10 +0200

    fixed getProviders function

--- app/(landing)/(authorization)/signin/page.tsx
+++ app/(landing)/(authorization)/signin/page.tsx
@@ -1,25 +1,17 @@
+import { getProviders } from "next-auth/react";
 import AuthContainer from "../_components/AuthContainer";
 import SignInButton from "../_components/SignInButton";
 
 const SignInPage = async () => {
-  // @TODO - getProviders()
-  let providers = [];
-  try {
-    const res = await fetch(
-      process.env.NEXT_PUBLIC_BASE_URL + "/api/auth/providers",
-      { cache: "no-store" }
-    );
-    providers = await res.json();
-  } catch (error) {
-    console.error(error);
-  }
+  let providers = await getProviders();
 
   return (
     <AuthContainer>
-      {Object.keys(providers).map((provider) => {
-        const p = providers[provider];
-        return <SignInButton key={p.name} id={p.id} provider={p.name} />;
-      })}
+      {providers &&
+        Object.keys(providers).map((provider) => {
+          const p = providers[provider];
+          return <SignInButton key={p.name} id={p.id} provider={p.name} />;
+        })}
     </AuthContainer>
   );
 };
